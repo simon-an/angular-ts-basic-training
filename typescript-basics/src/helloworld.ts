1933985eea681f140243985514370faedb11647e
Simon Potzernheim <nomisimon@posteo.de>
2018-09-17 18:39:11 +0200

    typescript wip

--- typescript-basics/src/helloworld.ts
+++ typescript-basics/src/helloworld.ts
@@ -1,1 +1,23 @@
-console.log('hello world') 
+interface BLA {
+    text: string;
+
+}
+
+const x: BLA = {
+    text: 'world'
+}
+console.log('hello', x.text);
+
+
+declare function create(o: object | null): void;
+
+
+declare function create(o: {} | null): void;
+
+declare function bla(x: Boolean): void;
+
+let someValue: unknown = "this is a string";
+
+if (typeof someValue === 'string') {
+    let strLength: number = (<string>someValue).length;
+}
