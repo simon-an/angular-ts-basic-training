efed377c92cd34e8c32b879e69530dec059da1ab
simon-an <nomisimon@posteo.de>
2018-11-02 17:15:16 +0100

    type assertion.

---
+++ typescript-basics/src/chapter2_strict_null_check.ts
@@ -0,0 +1,26 @@
+function createValue(): null | { value: number } {
+  const num = Math.random() * 10;
+  if (num > 8) {
+    return {
+      value: num
+    };
+  } else {
+    return null;
+  }
+}
+
+function checkNull(obj: any): boolean {
+  if (obj) return true;
+  return false;
+}
+
+while (true) {
+  const strictNull: null | { value: number } = createValue();
+  console.log(strictNull);
+  if (strictNull!.value) {
+    // if (checkNull(strictNull) && strictNull!.value) {
+    // if (strictNull.value) {
+    process.exit(strictNull!.value);
+    // process.exit(strictNull.value);
+  }
+}
