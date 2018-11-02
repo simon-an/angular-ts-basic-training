1d63c0e48235794b090e29bfc1645733d53fb5f6
simon-an <nomisimon@posteo.de>
2018-11-02 17:54:26 +0100

    add missing types

--- typescript-basics/src/chapter2_functions.ts
+++ typescript-basics/src/chapter2_functions.ts
@@ -2,7 +2,7 @@ function add(x: number, y: number): number {
   return x + y;
 }
 
-let myAdd = function (x: number, y: number): number {
+let myAdd = function(x: number, y: number): number {
   return x + y;
 };
 
@@ -14,25 +14,25 @@ let myAdd3: (x: number, y: number) => number = (x, y) => {
   return x + y;
 };
 
-function buildName(firstName: string, lastName?: string) {
-  if (lastName) return firstName + ' ' + lastName;
+function buildName(firstName: string, lastName?: string): string {
+  if (lastName) return firstName + " " + lastName;
   else return firstName;
 }
 
-let result1 = buildName('Bob'); // works correctly now
+let result1 = buildName("Bob"); // works correctly now
 // let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
-let result3 = buildName('Bob', 'Adams'); // ah, just right
+let result3 = buildName("Bob", "Adams"); // ah, just right
 
-function buildName2(firstName: string, lastName = 'Smith') {
-  return firstName + ' ' + lastName;
+function buildName2(firstName: string, lastName = "Smith"): string {
+  return firstName + " " + lastName;
 }
-let result4 = buildName2('Bob'); // works correctly now
+let result4 = buildName2("Bob"); // works correctly now
 
-function buildName3(firstName: string, ...restOfName: string[]) {
-  return firstName + ' ' + restOfName.join(' ');
+function buildName3(firstName: string, ...restOfName: string[]): string {
+  return firstName + " " + restOfName.join(" ");
 }
 
-let employeeName = buildName3('Joseph', 'Samuel', 'Lucas', 'MacKinzie');
+let employeeName = buildName3("Joseph", "Samuel", "Lucas", "MacKinzie");
 
 type functionType = (x: number, y: number) => number;
 let myAdd5: functionType = (x, y) => {
