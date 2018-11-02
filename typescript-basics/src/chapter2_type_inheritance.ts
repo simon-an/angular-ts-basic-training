da7ff5bde5ed45bfe6bdfef433d22833ef59147a
simon-an <nomisimon@posteo.de>
2018-11-02 17:43:50 +0100

    better type examples

---
+++ typescript-basics/src/chapter2_type_inheritance.ts
@@ -0,0 +1,5 @@
+/*  */
+
+function rollDie(): 1 | 2 | 3 | 4 | 5 | 6 {
+  return 1;
+}
---
+++ typescript-basics/src/chapter2_type_intersection.ts
@@ -0,0 +1,28 @@
+interface Square {
+  kind: "square";
+  size: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+type Shape = Square | Rectangle | Circle;
+
+function area(s: Shape) {
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.height * s.width;
+    case "circle":
+      return Math.PI * s.radius ** 2;
+  }
+}
+console.log(area({ kind: "square", size: 5 }));
+console.log(area({ kind: "rectangle", width: 1, height: 5 }));
