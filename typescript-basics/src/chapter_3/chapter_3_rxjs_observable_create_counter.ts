fbab5c1d5b89edc143a620abf4cc078f6c20dca9
simon-an <nomisimon@posteo.de>
2018-11-13 21:37:31 +0100

    fix(typescript): rxjs counter now shows interval is warm observable

--- typescript-basics/src/chapter_3/chapter_3_rxjs_observable_create_counter.ts
+++ typescript-basics/src/chapter_3/chapter_3_rxjs_observable_create_counter.ts
@@ -1,4 +1,5 @@
-import { interval } from "rxjs";
+import { take, switchMap } from "rxjs/operators";
+import { interval, of, timer } from "rxjs";
 
 // Create an Observable that will publish a value on an interval
 const secondsCounter = interval(1000);
@@ -6,3 +7,6 @@ const secondsCounter = interval(1000);
 secondsCounter.subscribe(n =>
   console.log(`It's been ${n} seconds since subscribing!`)
 );
+timer(5000)
+  .pipe(switchMap(() => secondsCounter))
+  .subscribe(n => console.log(`It's been ${n} seconds since subscribing!`));
